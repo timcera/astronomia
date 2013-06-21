fb19f8e39600c4baa6b96ed83084317b3df23fd9
Tim Cera <tim@cerazone.net>
2013-06-21 17:44:38 -0400

    Added tests that compare to Schureman.

--- tests/test_elp2000.py
+++ tests/test_elp2000.py
@@ -3,9 +3,13 @@ Tests for the elp2000 functions.
 '''
 
 from unittest import TestCase
+import datetime
+
+import numpy as np
 
 from astronomia import elp2000
 from astronomia.util import r_to_d
+from astronomia.calendar import cal_to_jd, hms_to_fday
 
 _elp = elp2000.ELP2000()
 
@@ -36,4 +40,60 @@ class TestELP(TestCase):
         self.assertAlmostEqual(calc_longitude - _comp_longitude, -4.05, places=0)
         self.assertAlmostEqual(calc_latitude, _comp_latitude, places=5)
         self.assertAlmostEqual(calc_radius, _comp_radius, places=1)
+
+    def test_compare_to_schureman(self):
+        rad2deg = 180.0/np.pi
+        dt = [datetime.datetime(i, 1, 1) for i in range(1800,2001,20)]
+        dt = dt + [datetime.datetime(1990,3,3,18,0,0)]
+        dt = dt + [datetime.datetime(1999,1,10,3,0,0)]
+        jd = [cal_to_jd(i.year, i.month, i.day) +
+              hms_to_fday(i.hour, i.minute, i.second) for i in dt]
+        jd = np.array(jd)
+        Nv = np.mod(_elp.mean_longitude_ascending_node(jd)*rad2deg, 360)
+        p = np.mod(_elp.mean_longitude_perigee(jd)*rad2deg, 360)
+        s = np.mod(_elp.mean_longitude(jd)*rad2deg, 360)
+
+        Nv_schureman = [33.25,
+                         6.47,
+                       339.64,
+                       312.81,
+                       285.98,
+                       259.16,
+                       232.38,
+                       205.55,
+                       178.72,
+                       151.89,
+                       125.07,
+                       318.45 - 3.12 - 0.11 - 0.04,
+                       144.39 + 0.00 - 0.48 - 0.01]
+        p_schureman = [225.45,
+                       319.15,
+                        52.96,
+                       146.77,
+                       240.58,
+                       334.38,
+                        68.08,
+                       161.88,
+                       255.69,
+                       349.49,
+                        83.29,
+                        36.45 + 6.57 + 0.22 + 0.08,
+                        42.63 + 0.00 + 1.00 + 0.01]
+        s_schureman = [342.31,
+                       102.71,
+                       236.29,
+                         9.87,
+                       143.45,
+                       277.03,
+                        37.43,
+                       171.01,
+                       304.59,
+                        78.16,
+                       211.74,
+                       np.mod(331.54 + 57.41 + 26.35 + 9.88, 360),
+                       82.36  + 0.00  + 118.59 + 1.65]
+        for i in range(len(Nv)):
+            self.assertAlmostEqual(Nv[i], Nv_schureman[i], places=1)
+            self.assertAlmostEqual(p[i], p_schureman[i], places=1)
+            self.assertAlmostEqual(s[i], s_schureman[i], places=1)
 
--- tests/test_sun.py
+++ tests/test_sun.py
@@ -3,11 +3,15 @@ Tests for the elp2000 functions.
 '''
 
 from unittest import TestCase
+import datetime
+
+import numpy as np
 
 from astronomia.constants import km_per_au
 from astronomia.sun import longitude_radius_low, apparent_longitude_low, aberration_low, Sun
 from astronomia.util import r_to_d, dms_to_d
 from astronomia.vsop87d import vsop_to_fk5, VSOP87d
+from astronomia.calendar import cal_to_jd, hms_to_fday
 
 sun = Sun()
 vsop = VSOP87d()
@@ -36,4 +40,43 @@ class TestSun(TestCase):
         self.assertAlmostEqual(r_to_d(B) * 3600 * 100, 0.72 * 100, delta=0.06)
         self.assertAlmostEqual(R, 0.99760853)
 
+    def test_compare_to_schureman(self):
+        rad2deg = 180.0/np.pi
+        dt = [datetime.datetime(i, 1, 1) for i in range(1800,2001,20)]
+        dt = dt + [datetime.datetime(1990,3,3,18,0,0)]
+        dt = dt + [datetime.datetime(1999,1,10,3,0,0)]
+        jd = [cal_to_jd(i.year, i.month, i.day) +
+              hms_to_fday(i.hour, i.minute, i.second) for i in dt]
+        jd = np.array(jd)
+        h = np.mod(sun.mean_longitude(jd)*rad2deg, 360)
+        p1 = np.mod(sun.mean_longitude_perigee(jd)*rad2deg, 360)
 
+        h_schureman = [280.41,
+                       279.57,
+                       279.73,
+                       279.88,
+                       280.04,
+                       280.19,
+                       279.36,
+                       279.51,
+                       279.67,
+                       279.82,
+                       279.97,
+                       280.39 + 58.15 + 1.97 + 0.74,
+                       280.21  + 0.00  + 8.87 + 0.12]
+        p1_schureman = [279.50,
+                       279.85,
+                       280.19,
+                       280.53,
+                       280.88,
+                       281.22,
+                       281.56,
+                       281.91,
+                       282.25,
+                       282.60,
+                       282.94,
+                       282.77 +  0.00 +  0.00 + 0.00,
+                       282.92  + 0.00  + 0.00 + 0.00]
+        for i in range(len(h)):
+            self.assertAlmostEqual(h[i], h_schureman[i], places=1)
+            self.assertAlmostEqual(p1[i], p1_schureman[i], places=1)
