ab32f01bafc47166ae32a0dacd017323bebfd36e
Tim Cera <tim@cerazone.net>
2020-03-10 07:31:12 -0400

    CI: Normalize to tstooolbox.

--- setup.py
+++ setup.py
@@ -2,62 +2,48 @@
 # -*- coding: utf-8 -*-
 
 import os
-import os.path
 import sys
+
+from setuptools import setup
 
 pkg_name = "astronomia"
 
 version = open("VERSION").readline().strip()
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
 if sys.argv[-1] == "publish":
     os.system("python setup.py sdist")
-
-    # The following block of code is to set the timestamp on files to
-    # 'now', otherwise ChromeOS/google drive sets to 1970-01-01 and then
-    # no one can install it because zip doesn't support dates before
-    # 1980.
-    os.chdir("dist")
-    os.system("tar xvzf {pkg_name}-{version}.tar.gz".format(**locals()))
-    os.system("find {pkg_name}-{version}* -exec touch {{}} \\;".format(**locals()))
-    os.system(
-        "tar czf {pkg_name}-{version}.tar.gz {pkg_name}-{version}".format(**locals())
-    )
-    shutil.rmtree("{pkg_name}-{version}".format(**locals()))
-    os.chdir("..")
-
     os.system("twine upload dist/{pkg_name}-{version}.tar.gz".format(**locals()))
     sys.exit()
 
-readme = open("README.rst").read()
+README = open("README.rst").read()
 
 install_requires = [
     # List your project dependencies here.
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
-    "mando >= 0.4",
     "tstoolbox",
 ]
 
 setup(
-    name="astronomia",
-    version=open("VERSION").readline().strip(),
+    name=pkg_name,
+    version=version,
     description="Library for calculation of ephemeris and other astronomical calculations",
-    long_description=readme + "\n\n",
+    long_description=README,
     classifiers=[
+        # Get strings from
+        # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering :: Astronomy",
     ],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords="ephemeris astronomy",
@@ -69,6 +55,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
+    entry_points={"console_scripts": ["astronomia=astronomia.astronomia:main"]},
     scripts=[
         "apps/solstice.py",
         "apps/check_perihelion.py",
@@ -76,8 +63,12 @@ setup(
         "apps/easter-cgi.py",
         "apps/solstice-cgi.py",
     ],
-    data_files=[(os.path.join("share", "astronomia"), ["apps/astronomia_params.txt"])],
-    entry_points={"console_scripts": ["astronomia=astronomia.astronomia:main"]},
+    data_files=[
+        (
+            os.path.join("share", "astronomia"),
+            os.path.join("apps", "astronomia_params.txt"),
+        )
+    ],
     test_suite="tests",
     python_requires=">=3.6",
 )
